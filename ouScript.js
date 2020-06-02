1dddc5449b2e64995acc869377621e2f3e803c27
StephanieKeown <65156199+StephanieKeown@users.noreply.github.com>
2020-06-02 21:27:06 +0100

    First Wireframe
    Features javascript and css

---
+++ ouScript.js
@@ -0,0 +1,72 @@
+// JavaScript source code
+
+
+function fnAddWalk() {
+	
+	var x = document.getElementById("tblWalksInput");
+	  if (x.style.display === "none") {
+		x.style.display = "block";
+	  } else {
+		x.style.display = "none";
+	  }
+	  
+}
+
+function fnSubmitWalk() {
+	
+	var x = document.getElementById("tblWalksInput");
+	  if (x.style.display === "block") {
+		x.style.display = "none";
+	  } else {
+		x.style.display = "block";
+	  }
+	  
+	}
+	
+	
+	
+function fnViewWalks() {
+	
+	var x = document.getElementById("tblWalksView");
+	  if (x.style.display === "block") {
+		x.style.display = "none";
+	  } else {
+		x.style.display = "block";
+	  }
+	  	
+}
+
+
+function fnViewSocialEvents() {
+	
+	var x = document.getElementById("tblSocialEvents");
+	  if (x.style.display === "block") {
+		x.style.display = "none";
+	  } else {
+		x.style.display = "block";
+	  }
+	  	
+}
+
+function fnViewMembers() {
+	
+	var x = document.getElementById("tblMembers");
+	  if (x.style.display === "block") {
+		x.style.display = "none";
+	  } else {
+		x.style.display = "block";
+	  }
+	  	
+}
+
+
+function fnClubAnnouncements() {
+	
+	var x = document.getElementById("tblClubAnnouncements");
+	  if (x.style.display === "block") {
+		x.style.display = "none";
+	  } else {
+		x.style.display = "block";
+	  }
+	  	
+}
